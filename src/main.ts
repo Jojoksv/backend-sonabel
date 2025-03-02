8f3e31d2778c126a1c7eecc97bb4df0e0b097029
Joseph Kossouvi <jkossouvi@gmail.com>
2025-03-02 17:45:27 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { origin } from "./config/constants/index.ts"
+import { origin } from "./config/constants/index"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
